8f1fe5c6f35bcf8fd46c8627c5d94259320eb4c6
quyenpq <quyenpq.work@gmail.com>
2026-01-02 01:02:47 +0700

    fix: update websocket url

--- lib/socket.ts
+++ lib/socket.ts
@@ -11,7 +11,7 @@ export interface ClientToServerEvents {
   leaveRoom: (roomId: string) => void;
 }
 
-const URL = "http://localhost:3000";
+const URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
   URL,
